7216dacc4a585b232f265ceac359ea8fe222bdb3
Mustafa Paksoy <paksoy@gmail.com>
2013-04-06 19:01:18 -0700

    List links.

--- follow.js
+++ follow.js
@@ -1,26 +1,28 @@
-// List following and followers from several accounts
-
-var users = ['PhantomJS',
-        'ariyahidayat',
-        'detronizator',
-        'KDABQt',
-        'lfranchi',
-        'jonleighton',
-        '_jamesmgreene',
-        'Vitalliumm'];
+var users = ['gold-coast'];
 
 function follow(user, callback) {
     var page = require('webpage').create();
-    page.open('http://chicago.flywheelsports.com/reserve/gold-coast', function (status) {
+    page.open('http://chicago.flywheelsports.com/reserve/' + user, function (status) {
         if (status === 'fail') {
             console.log(user + ': ?');
         } else {
             var data = page.evaluate(function () {
-                return document.querySelectorAll('#schedule_classes a');
+                var links = document.querySelectorAll('#schedule_classes a');
+                var ret = [];
+                for (var i = 0; i < links.length; i++) {
+                    ret.push(links[i].href);
+                }
+                return ret;
             });
             console.log(user + ': ' + data.length);
+            console.log(user + ': ' + data[0]);
+            console.log(user + ': ' + data[1]);
         }
         page.close();
+
+        if (callback) {
+            callback();
+        }
     });
 }
 
