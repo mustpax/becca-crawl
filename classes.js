f19e5d759b43ce31f39156c02a083f36b3429f0f
Mustafa Paksoy <paksoy@gmail.com>
2013-04-07 11:30:51 -0700

    Add all the locations.

--- classes.js
+++ classes.js
@@ -1,8 +1,36 @@
-var users = ['http://chicago.flywheelsports.com/reserve/gold-coast',
-             'http://chicago.flywheelsports.com/reserve/highland-park'];
+var users = ["http://florida.flywheelsports.com/reserve/miami-beach",
+             "http://florida.flywheelsports.com/reserve/boca",
+             "http://florida.flywheelsports.com/flybarre-schedule/boca",
+             "http://new-york.flywheelsports.com/reserve/flatiron",
+             "http://new-york.flywheelsports.com/reserve/jcc",
+             "http://new-york.flywheelsports.com/reserve/upper-east-side",
+             "http://new-york.flywheelsports.com/reserve/sag-harbor",
+             "http://new-york.flywheelsports.com/reserve/east-hampton",
+             "http://new-york.flywheelsports.com/reserve/millburn",
+             "http://new-york.flywheelsports.com/reserve/stamford",
+             "http://new-york.flywheelsports.com/reserve/englewood",
+             "http://new-york.flywheelsports.com/flybarre-schedule/flatiron",
+             "http://new-york.flywheelsports.com/flybarre-schedule/east-hampton",
+             "http://new-york.flywheelsports.com/flybarre-schedule/millburn",
+             "http://chicago.flywheelsports.com/reserve/gold-coast",
+             "http://chicago.flywheelsports.com/reserve/highland-park",
+             "http://chicago.flywheelsports.com/flybarre-schedule/highland-park",
+             "http://seattle.flywheelsports.com/reserve/south-lake-union",
+             "http://seattle.flywheelsports.com/reserve/bellevue",
+             "http://seattle.flywheelsports.com/flybarre-schedule/south-lake-union",
+             "http://seattle.flywheelsports.com/flybarre-schedule/bellevue",
+             "http://texas.flywheelsports.com/reserve/highland-park",
+             "http://texas.flywheelsports.com/reserve/plano",
+             "http://los-angeles.flywheelsports.com/reserve/west-hollywood",
+             "http://los-angeles.flywheelsports.com/reserve/larchmont",
+             "http://philadelphia.flywheelsports.com/reserve/center-city",
+             "http://dubai.flywheelsports.com/reserve/burj-views"];
+
+var webpage = require('webpage');
+var system  = require('system');
 
 function follow(user, callback) {
-    var page = require('webpage').create();
+    var page = webpage.create();
     page.open(user, function (status) {
         if (status === 'fail') {
             console.log(user + ': fail');
@@ -16,7 +44,7 @@ function follow(user, callback) {
                 return ret;
             });
             for (var i = 0; i < data.length; i++) {
-                console.log(user + ' ' + data[i]);
+                console.log(user + '\t' + data[i]);
             }
         }
         page.close();
@@ -29,8 +57,7 @@ function follow(user, callback) {
 
 function process() {
     if (users.length > 0) {
-        var user = users[0];
-        users.splice(0, 1);
+        var user = users.pop();
         follow(user, process);
     } else {
         phantom.exit();
