64f10cb63dbc5af3c45e01a2c942e5fd6f5966c4
Mustafa Paksoy <paksoy@gmail.com>
2013-04-23 20:02:26 -0700

    Script for automating job runs.

---
+++ run.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# Fetch last week's classes
+
+CLASS_SCRIPT        ='classes.js'
+ATTENDANT_SCRIPT    ='attendant.js'
+PHANTOM_JS          ='phantomjs'
+
+from subprocess import Popen, PIPE
+
+LOCATIONS = ["http://florida.flywheelsports.com/reserve/miami-beach",
+             "http://florida.flywheelsports.com/reserve/boca",
+             "http://new-york.flywheelsports.com/reserve/flatiron",
+             "http://new-york.flywheelsports.com/reserve/jcc",
+             "http://new-york.flywheelsports.com/reserve/upper-east-side",
+             "http://new-york.flywheelsports.com/reserve/sag-harbor",
+             "http://new-york.flywheelsports.com/reserve/east-hampton",
+             "http://new-york.flywheelsports.com/reserve/millburn",
+             "http://new-york.flywheelsports.com/reserve/stamford",
+             "http://new-york.flywheelsports.com/reserve/englewood",
+             "http://chicago.flywheelsports.com/reserve/gold-coast",
+             "http://chicago.flywheelsports.com/reserve/highland-park",
+             "http://seattle.flywheelsports.com/reserve/south-lake-union",
+             "http://seattle.flywheelsports.com/reserve/bellevue",
+             "http://texas.flywheelsports.com/reserve/highland-park",
+             "http://texas.flywheelsports.com/reserve/plano",
+             "http://los-angeles.flywheelsports.com/reserve/west-hollywood",
+             "http://los-angeles.flywheelsports.com/reserve/larchmont",
+             "http://philadelphia.flywheelsports.com/reserve/center-city",
+             "http://dubai.flywheelsports.com/reserve/burj-views"]
+
+def get_classes():
+    process = Popen(' '.join([PHANTOM_JS, CLASS_SCRIPT, LOCATIONS[0]]),
+                    shell=True,
+                    stdout=PIPE)
+    return [process.stdout.readline().strip().split('\t')[1]]
+
+
+def main():
+    classes = get_classes()
+    for cls in classes:
+        print cls
+
+if __name__ == '__main__':
+    main()
+
