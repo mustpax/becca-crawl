587aa10f154d703ef17e067d467d435de7ee2c5d
Mustafa Paksoy <paksoy@gmail.com>
2013-04-23 19:59:55 -0700

    Read class URLs from cl args.

--- classes.js
+++ classes.js
@@ -1,26 +1,7 @@
-var users = ["http://florida.flywheelsports.com/reserve/miami-beach",
-             "http://florida.flywheelsports.com/reserve/boca",
-             "http://new-york.flywheelsports.com/reserve/flatiron",
-             "http://new-york.flywheelsports.com/reserve/jcc",
-             "http://new-york.flywheelsports.com/reserve/upper-east-side",
-             "http://new-york.flywheelsports.com/reserve/sag-harbor",
-             "http://new-york.flywheelsports.com/reserve/east-hampton",
-             "http://new-york.flywheelsports.com/reserve/millburn",
-             "http://new-york.flywheelsports.com/reserve/stamford",
-             "http://new-york.flywheelsports.com/reserve/englewood",
-             "http://chicago.flywheelsports.com/reserve/gold-coast",
-             "http://chicago.flywheelsports.com/reserve/highland-park",
-             "http://seattle.flywheelsports.com/reserve/south-lake-union",
-             "http://seattle.flywheelsports.com/reserve/bellevue",
-             "http://texas.flywheelsports.com/reserve/highland-park",
-             "http://texas.flywheelsports.com/reserve/plano",
-             "http://los-angeles.flywheelsports.com/reserve/west-hollywood",
-             "http://los-angeles.flywheelsports.com/reserve/larchmont",
-             "http://philadelphia.flywheelsports.com/reserve/center-city",
-             "http://dubai.flywheelsports.com/reserve/burj-views"];
-
 var webpage = require('webpage');
 var system  = require('system');
+
+var users = system.args.slice(1);
 
 function follow(user, callback) {
     user = user + '/prev';
